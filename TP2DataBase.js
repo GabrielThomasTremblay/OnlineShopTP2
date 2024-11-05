c2545dec35619ba597ad4b91aad10368bc260ad9
Gabriel Thomas Tremblay <2330009@collegebart.ca>
2024-11-05 11:43:03 -0500

    Database created? (didn't test it yet so...) & Login box in html

--- TP2DataBase.js
+++ TP2DataBase.js
@@ -1,11 +1,30 @@
-function openLogIn(){
+function openLogIn(username, password){
     
     let openRequest = indexedDB.open("Users")
 
+    openRequest.onupgradeneeded = function(){
+
+        let UserDataBase = openRequest.result;
+        UserDataBase.createObjectStore('ListOfCurrentUsers', {keyPath: 'username'});
+
+        UserDataBase.createIndex("username", "username", {unique: true});
+        UserDataBase.createIndex("password", "password", {unique: false});
+        UserDataBase.createIndex("JSONData", "JSONData", {unique: true});
+    }
     openRequest.onerror = function(){
-        console.log("Problème en ouvrant la base de donné");
+
+        console.log("Probleme en ouvrant la base de donne");
     }
     openRequest.onsuccess = function(){
-        console.log("Base de donner ouverte avec succès");
+
+        console.log("Base de donner ouverte avec succes");
+        let UserDataBase = openRequest.result;
+
+        let transaction = UserDataBase.transaction("Users", "readwrite");
+
+        let ListOfCurentUsers = transaction.objectStrore("ListOfCurrentUsers");
     }
+
+
+
 }
