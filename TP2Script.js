0b363ae20fae252fe8813cc454d1cba411908404
GabrielThomasTremblay <2330009@bart.ca>
2024-11-28 15:30:19 -0500

    The shop is working perfectly

--- TP2Script.js
+++ TP2Script.js
@@ -69,8 +69,64 @@ function openLogIn(Curr_Username, Curr_Password){
     }
 }
 let Token = 0;
+let Mult = 1.0;
+let CatFood = 0;
+let BlueYarn = false;
+let BlueToad = false;
 function Click(){
     let VisToken = document.getElementById("Token");
-    Token = Token + 1;
+    ActiveMult();
+    Token = Token + ((1 + CatFood) * Mult);
+    Token = Math.round(Token*100)/100;
     VisToken.textContent = Token
+}
+function ActiveMult(){
+    if(BlueYarn == true){
+        let itemprice = document.getElementById("Item2");
+        itemprice.textContent = "Bought";
+
+        Mult = Mult + 0.1;
+    }
+    if(BlueToad == true){
+        let itemprice = document.getElementById("Item3");
+        itemprice.textContent = "Bought";
+        
+        Mult = Mult + 0.5;
+    }
+}
+function UpdatePrice(Price){
+    let VisToken = document.getElementById("Token");
+    Token = Token - Price;
+    Token = Math.round(Token*100)/100;
+    VisToken.textContent = Token;
+}
+function UpdateCat(Image){
+    let cat = document.getElementById("Cat");
+    cat.style.backgroundImage="url("+Image+")";
+}
+function BuyItem(Curr_Item){
+
+    switch(Curr_Item){
+        case "Item1":
+            if(Token >= 10){
+                UpdatePrice(10);
+                CatFood = CatFood + 1;
+                break;
+            }
+        case "Item2":
+            if(Token >= 50 && BlueYarn == false){
+                UpdatePrice(50);
+                BlueYarn = true;
+                ActiveMult();
+                break;
+            }
+        case "Item3":
+            if(Token >= 200 && BlueToad == false){
+                UpdatePrice(200);
+                BlueToad = true;
+                ActiveMult();
+                UpdateCat('https://static.wikia.nocookie.net/marioinlife/images/2/28/Cat_Toad_Artwork_-_Super_Mario_3D_World.png/revision/latest?cb=20210304154856');
+                break;
+            }
+    }
 }
